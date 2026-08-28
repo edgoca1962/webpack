5c13aa4b82f9a58fe2949f017ecdd5903419d26f
Edwin Gonzalez <edgoca1962@gmail.com>
2026-08-27 18:14:00 -0600

    Ajuste SGF a FRW

---
+++ src/sourceSGF.js
@@ -0,0 +1,21 @@
+import * as bootstrap from 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'animate.css'
+import fontawesome from '@fortawesome/fontawesome-free/css/all.min.css'
+import swal from 'sweetalert2';
+window.Swal = swal;
+
+import corecss from './css/core.css'
+import scss from './css/core.scss'
+import borderscss from './css/animacion-bordes.css'
+
+import billetera from './js/billetera'
+import calendario from './js/calendario'
+import core from './js/core'
+import csvfile from './js/csvfile'
+import libros from './js/libros'
+
+import perfilusuario from './js/perfilusuario'
+import presupuesto from './js/presupuesto'
+import sgf from './js/sgf'
+import tablero from './js/tablero'
